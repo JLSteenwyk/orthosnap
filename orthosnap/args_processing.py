70de00ea68690e6696f91eb4e165a7d5a8c12791
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-03-03 05:09:31 -0800

    added check for appropriate occupancy threshold value

--- orthosnap/args_processing.py
+++ orthosnap/args_processing.py
@@ -23,4 +23,10 @@ def process_args(args) -> dict:
     support = args.support if args.support is not None else 80
     occupancy = args.occupancy if args.occupancy is not None else 1.0
 
+    if occupancy > 1 or occupancy < 0:
+        logger.warning("Occupancy threshold must range from 0 to 1.")
+        logger.warning("I recommend using a high threshold close to one.")
+        logger.warning("For example, 0.5, 0.75, or 1.")
+        sys.exit()
+
     return dict(tree=tree, fasta=fasta, support=support, occupancy=occupancy)
