772cb5ac5cdf4169e9ecddb85f8f809625ad2697
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-03-03 17:00:46 -0800

    added github actions commands used during CI

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -10,7 +10,7 @@ here = Path(__file__)
 
 @pytest.mark.integration
 class TestIntegration(object):
-    def test_simple_no_change(self):
+    def test_default_param_OG0000010(self):
         """
         """
         kwargs = dict(
@@ -22,7 +22,7 @@ class TestIntegration(object):
         execute(**kwargs)
 
         with open(
-            f"{here.parent.parent}/expected/OG0000010.renamed.fa.mafft.clipkit.orthosnap.0.fa", "r"
+            f"{here.parent.parent}/expected/test_default_param_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.0.fa", "r"
         ) as expected:
             expected_content = expected.read()
 
@@ -32,7 +32,7 @@ class TestIntegration(object):
         assert expected_content == output_content
 
         with open(
-            f"{here.parent.parent}/expected/OG0000010.renamed.fa.mafft.clipkit.orthosnap.1.fa", "r"
+            f"{here.parent.parent}/expected/test_default_param_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.1.fa", "r"
         ) as expected:
             expected_content = expected.read()
 
@@ -42,7 +42,7 @@ class TestIntegration(object):
         assert expected_content == output_content
 
         with open(
-            f"{here.parent.parent}/expected/OG0000010.renamed.fa.mafft.clipkit.orthosnap.2.fa", "r"
+            f"{here.parent.parent}/expected/test_default_param_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.2.fa", "r"
         ) as expected:
             expected_content = expected.read()
 
@@ -52,7 +52,7 @@ class TestIntegration(object):
         assert expected_content == output_content
 
         with open(
-            f"{here.parent.parent}/expected/OG0000010.renamed.fa.mafft.clipkit.orthosnap.3.fa", "r"
+            f"{here.parent.parent}/expected/test_default_param_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.3.fa", "r"
         ) as expected:
             expected_content = expected.read()
 
@@ -62,11 +62,123 @@ class TestIntegration(object):
         assert expected_content == output_content
 
         with open(
-            f"{here.parent.parent}/expected/OG0000010.renamed.fa.mafft.clipkit.orthosnap.4.fa", "r"
+            f"{here.parent.parent}/expected/test_default_param_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.4.fa", "r"
         ) as expected:
             expected_content = expected.read()
 
         with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.4.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+
+    def test_occupancy_two_tenths_OG0000010(self):
+        """
+        """
+        kwargs = dict(
+            tree=f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.treefile",
+            fasta=f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit",
+            support=80,
+            occupancy=.2,
+        )
+        execute(**kwargs)
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.0.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.0.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.1.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.1.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.2.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.2.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.3.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.3.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.4.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.4.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.5.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.5.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.6.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.6.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.7.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.7.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.8.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.8.fa", "r") as out_file:
+            output_content = out_file.read()
+
+        assert expected_content == output_content
+
+        with open(
+            f"{here.parent.parent}/expected/test_occupancy_two_tenths_OG0000010/OG0000010.renamed.fa.mafft.clipkit.orthosnap.9.fa", "r"
+        ) as expected:
+            expected_content = expected.read()
+
+        with open(f"{here.parent.parent}/samples/OG0000010.renamed.fa.mafft.clipkit.orthosnap.9.fa", "r") as out_file:
             output_content = out_file.read()
 
         assert expected_content == output_content
